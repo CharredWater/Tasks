ee977f658ecda0ec1114936b22650c8c1ad2bf8e
Nicholas Lau <nicholaslau@lib-dhcp-187.ci.mtnview.ca.us>
2013-10-30 18:23:28 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasks::Application.config.secret_key_base = 'a01859936b5e1e774cdcf1b767f9a3d5b6814c07a8fdb721a2dc47df3a093125a463af36cd6e40a7698bb5ba24c1451dc4e919d4901c62a0e1a33e6476ed64f9'
